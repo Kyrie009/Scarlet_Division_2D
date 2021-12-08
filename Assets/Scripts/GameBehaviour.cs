c73b0f67f1ee5413fb6830b9766c790ca62ab7a2
Kyrie009 <dbui1@myjmc.edu.au>
2021-12-08 11:21:40 +1100

    w10

--- Assets/Scripts/GameBehaviour.cs
+++ Assets/Scripts/GameBehaviour.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameBehaviour : MonoBehaviour
 {
         //Managers
-        //protected static GameManager _GM { get { return GameManager.INSTANCE; } }
+        protected static GameManager _GM { get { return GameManager.INSTANCE; } }
      
 
 
@@ -29,3 +29,44 @@ public class GameBehaviour : MonoBehaviour
             return _list;
         }
  }
+
+public class Singleton<T> : GameBehaviour where T : MonoBehaviour
+{
+    public bool dontDestroy;
+    private static T instance_;
+    public static T INSTANCE
+    {
+        get
+        {
+            if (instance_ == null)
+            {
+                instance_ = GameObject.FindObjectOfType<T>();
+                if (instance_ == null)
+                {
+                    GameObject singleton = new GameObject(typeof(T).Name);
+                    singleton.AddComponent<T>();
+                }
+            }
+            return instance_;
+        }
+    }
+    protected virtual void Awake()
+    {
+        if (instance_ == null)
+        {
+            instance_ = this as T;
+            if (dontDestroy)
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
+
+
---
+++ Assets/Scripts/GameEvents.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+public static class GameEvents 
+{
+    //GameStates
+    //public static event Action OnStart = null;
+
+    //functions reported to by the scripts
+    public static void ReportGameStart()
+    {
+        
+    }
+}
